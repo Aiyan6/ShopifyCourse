5771f72643f7586786e26dbce96bff3df7174736
Aiyan6 <aiyantufail@hotmail.com>
2024-10-17 14:38:31 +0500

    [FEATURE]: 3D Models

--- assets/product-model.js
+++ assets/product-model.js
@@ -25,7 +25,9 @@ class ProductModel extends HTMLElement {
 
     openModalButton.addEventListener("click", function (e) {
       modal.querySelector("#body").innerHTML = "";
-      const template = document.querySelector(`product-model[data-media-id=${mediaID }] > template`);
+      const template = document.querySelector(
+        `product-model[data-media-id="${mediaID}"] > template`
+      );
       console.log(template);
     });
   }
